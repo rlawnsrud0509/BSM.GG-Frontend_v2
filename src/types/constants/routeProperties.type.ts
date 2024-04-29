1eaa0b18d1d44999d6984bf8b876adf50ec7feeb
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-04-30 00:12:34 +0900

    feat: 동적 키값 적용을 위해 라우팅 속성 타입 명시

---
+++ src/types/constants/routeProperties.type.ts
@@ -0,0 +1,13 @@
+export interface routeProperties {
+  [key: string]: string;
+  HOME: string;
+  RANKING: string;
+  LOLNERD: string;
+  REGISTER_ACCOUNT: string;
+}
+
+export interface routeNameProperties {
+  [key: string]: string;
+  RANKING: string;
+  LOLNERD: string;
+}
