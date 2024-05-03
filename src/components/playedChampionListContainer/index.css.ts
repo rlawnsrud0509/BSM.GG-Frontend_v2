68a056cfb094a4fd106636fb8f75c676daabf0d1
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-03 12:13:42 +0900

    feat: 플레이한 챔피언 목록 컨테이너 퍼블리싱

---
+++ src/components/playedChampionListContainer/index.css.ts
@@ -0,0 +1,38 @@
+import { font } from "@/style/base/font";
+import { theme } from "@/style/base/theme/index.css";
+import { Column } from "@/style/ui/Flex";
+import { style } from "@vanilla-extract/css";
+
+export const Container = style(
+  Column({
+    width: "30rem",
+    position: "relative",
+
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+
+    padding: "1rem",
+    gap: "1rem",
+    boxSizing: "border-box",
+    backgroundColor: theme.base.white,
+
+    borderRadius: "0.5rem",
+    boxShadow: `0 0 0.8rem ${theme.gray[200]}`,
+  }),
+);
+
+export const PlayedListText = style({
+  ...font["md"],
+  color: theme.gray[800],
+});
+
+export const PlayedChampionListSection = style(
+  Column({
+    width: "100%",
+    padding: "1rem",
+    gap: "1rem",
+
+    boxSizing: "border-box",
+    backgroundColor: theme.base.white,
+  }),
+);
