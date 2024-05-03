b013613b9a263d8980377930e2ed732bff48514e
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-03 12:16:15 +0900

    feat: 유저 랭크 정보 컨테이너 퍼블리싱

---
+++ src/components/RankInfoContainer/index.css.ts
@@ -0,0 +1,60 @@
+import { font } from "@/style/base/font";
+import { theme } from "@/style/base/theme/index.css";
+import { Column, Row } from "@/style/ui/Flex";
+import { style } from "@vanilla-extract/css";
+
+export const Container = style(
+  Column({
+    width: "30rem",
+    position: "relative",
+
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+
+    padding: "1rem",
+    boxSizing: "border-box",
+    backgroundColor: theme.base.white,
+
+    borderRadius: "0.5rem",
+    boxShadow: `0 0 0.8rem ${theme.gray[200]}`,
+  }),
+);
+
+export const RankTypeText = style({
+  ...font["md"],
+  color: theme.gray[800],
+});
+
+export const RankDetailInfoSection = style(
+  Row({
+    width: "100%",
+    padding: "1.6rem",
+    gap: "2rem",
+
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  }),
+);
+
+export const RankIcon = style({
+  width: "7rem",
+  height: "7rem",
+
+  borderRadius: "0.5rem",
+  backgroundColor: theme.gray[200],
+});
+
+export const RankTextBox = style(
+  Column({
+    alignItems: "flex-start",
+    gap: "0.4rem",
+  }),
+);
+
+export const RankText = style({
+  ...font["xl"],
+});
+
+export const RankPointText = style({
+  ...font["md"],
+});
