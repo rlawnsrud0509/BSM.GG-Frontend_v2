eafe4b591d1caf1b052711400384263cf3b061a4
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-04 19:53:28 +0900

    feat: 게임 종류 구분 헤더 추가

---
+++ src/components/gameRecordContainer/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import GameRecordHeaderLink from "./gameRecordHeaderLink";
+import * as S from "./index.css";
+import { GAME_RECORD_HEADER } from "@/constants/gameRecordHeader";
+import GameRecordPreviewContainer from "./gameRecordInfoContainer/gameRecordPreviewContainer";
+import GameRecordInfoContainer from "./gameRecordInfoContainer";
+
+const GameRecordContainer = () => {
+  const [selectedLink, setSelectedLink] = useState(GAME_RECORD_HEADER.전체);
+
+  return (
+    <div className={S.Container}>
+      <div className={S.gameRecordHeader}>
+        {Object.keys(GAME_RECORD_HEADER).map((e) => (
+          <GameRecordHeaderLink
+            onClick={() => setSelectedLink(e)}
+            linkText={e}
+            selectedLink={selectedLink}
+          />
+        ))}
+      </div>
+      <GameRecordInfoContainer />
+      <GameRecordInfoContainer />
+      <GameRecordInfoContainer />
+      <GameRecordInfoContainer />
+      <GameRecordInfoContainer />
+      <GameRecordInfoContainer />
+    </div>
+  );
+};
+
+export default GameRecordContainer;
