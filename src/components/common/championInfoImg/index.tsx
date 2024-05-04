9ec26e4ebd73b8f85d4506819a29238986cb6439
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-04 19:51:19 +0900

    refactor: 챔피언 아이콘&레벨 보여주는 컴포넌트 분리

---
+++ src/components/common/championInfoImg/index.tsx
@@ -0,0 +1,26 @@
+import { ChampionInfoImgProperties } from "@/types/components/ChampionInfoImgProperties.type";
+import * as S from "./index.css";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+
+const ChampionInfoImg = ({ championName, level, containerSize }: ChampionInfoImgProperties) => {
+  return (
+    <div
+      className={S.Container}
+      style={assignInlineVars({
+        [S.containerSize]: `${containerSize}rem`,
+      })}
+    >
+      <img src={championName} alt="ff" />
+      <div
+        className={S.ChampionLevel}
+        style={assignInlineVars({
+          [S.positionValue]: `${(containerSize - 3) * 1}%`,
+        })}
+      >
+        {level}
+      </div>
+    </div>
+  );
+};
+
+export default ChampionInfoImg;
