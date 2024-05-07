b1cdee023274ef8c0b0472bc7ba2556a3fe4126d
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-07 16:14:44 +0900

    feat: 교내랭킹 페이지 퍼블리싱

---
+++ src/app/ranking/page.css.ts
@@ -0,0 +1,23 @@
+import { font } from "@/style/base/font";
+import { Column } from "@/style/ui/Flex";
+import { style } from "@vanilla-extract/css";
+
+export const Container = style(
+  Column({
+    width: "100%",
+    position: "relative",
+
+    justifyContent: "flex-start",
+    padding: "16rem 0",
+    boxSizing: "border-box",
+
+    overflowY: "scroll",
+    gap: "0.6rem",
+  }),
+);
+
+export const PageText = style({
+  width: "80%",
+  marginBottom: "4rem",
+  ...font["3xl"],
+});
