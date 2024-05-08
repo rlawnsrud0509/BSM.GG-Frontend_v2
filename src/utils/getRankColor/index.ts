f44b21b53e43e0a7023d8f191d3a6cbeee51255e
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-08 14:51:13 +0900

    feat: 랭킹에 따른 컨테이너 색 반환 함수 추가

---
+++ src/utils/getRankColor/index.ts
@@ -0,0 +1,18 @@
+import { theme } from "@/style/base/theme/index.css";
+
+export const getRankColor = (rank: number) => {
+  switch (true) {
+    case rank === 1:
+      return theme.primary[500];
+    case rank === 2:
+      return theme.primary[300];
+    case rank === 3:
+      return theme.primary[200];
+    case rank >= 14:
+      return theme.gray[50];
+    case rank >= 9:
+      return theme.primary[50];
+    case rank >= 4:
+      return theme.primary[100];
+  }
+};
