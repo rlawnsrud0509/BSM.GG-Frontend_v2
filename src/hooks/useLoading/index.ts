20409a6490489df38f2960f91a03be53defa3885
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-20 10:01:11 +0900

    feat: useLoading 훅 추가

---
+++ src/hooks/useLoading/index.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export const useLoading = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  return isLoading;
+};
