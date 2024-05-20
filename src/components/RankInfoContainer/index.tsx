e99238e783b27216326ae83aa6be160c50e721b5
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-20 19:39:37 +0900

    feat: 전적검색 페이지 랭크 정보 컨테이너 api 연결

--- src/components/RankInfoContainer/index.tsx
+++ src/components/RankInfoContainer/index.tsx
@@ -1,14 +1,33 @@
+"use client";
+
+import { useUserParams } from "@/hooks/useUserParams";
 import * as S from "./index.css";
+import { useGetUserFlexRankInfoQuery, useGetUserSoloRankInfoQuery } from "@/service/record/graphql";
 
 const RankInfoContainer = ({ rankTypeText }: { rankTypeText: string }) => {
+  const rankField = rankTypeText === "솔로랭크" ? "solo_tier" : "solo_lp";
+  const lpField = rankTypeText === "솔로랭크" ? "solo_lp" : "flex_lp";
+
+  const userParams = useUserParams();
+  const { data } =
+    rankTypeText === "솔로랭크"
+      ? useGetUserSoloRankInfoQuery(userParams)
+      : useGetUserFlexRankInfoQuery(userParams);
+
   return (
     <main className={S.Container}>
       <div className={S.RankTypeText}>{rankTypeText}</div>
       <section className={S.RankDetailInfoSection}>
         <div className={S.RankIcon} />
         <div className={S.RankTextBox}>
-          <span className={S.RankText}>Diamond 3</span>
-          <span className={S.RankPointText}>99 lp</span>
+          {data.summoner.summoner_types[0][rankField] ? (
+            <>
+              <span className={S.RankText}>{data.summoner.summoner_types[0][rankField]}</span>
+              <span className={S.RankPointText}>{data.summoner.summoner_types[0][lpField]} lp</span>
+            </>
+          ) : (
+            <span className={S.RankText}>Unranked</span>
+          )}
         </div>
       </section>
     </main>
