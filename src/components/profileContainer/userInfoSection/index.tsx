77fb195199aa681b790c958f120f8552e65fd046
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-20 19:37:28 +0900

    feat: 전적검색 페이지 프로필 컨테이너 api연결

--- src/components/profileContainer/userInfoSection/index.tsx
+++ src/components/profileContainer/userInfoSection/index.tsx
@@ -1,15 +1,31 @@
+import { UserInfoSectionProperties } from "@/types/components/userInfoSectionProperties.type";
 import * as S from "./index.css";
+import { getFormattedClassNumber } from "@/utils/getFormattedClassNumber";
+import { getUserPercentage } from "@/utils/getUserPercentage";
 
-const UserinfoSection = () => {
+const UserinfoSection = ({
+  game_name,
+  tag_line,
+  ranking,
+  name,
+  grade,
+  class_no,
+  student_no,
+  user_count,
+}: UserInfoSectionProperties) => {
   return (
     <div className={S.Container}>
       <div className={S.UserDetailInfoSection}>
-        <span className={S.UserLevelText}>3106 김준경</span>
-        <span className={S.UserRankingText}>교내 랭킹 34위 (상위 28.6%)</span>
+        <span className={S.UserLevelText}>
+          {"" + grade + class_no + getFormattedClassNumber(student_no) + " " + name}
+        </span>
+        <span className={S.UserRankingText}>
+          교내 랭킹 {ranking}위 (상위 {getUserPercentage(ranking, user_count)})
+        </span>
       </div>
       <div className={S.UserNameSection}>
-        <span className={S.UserNameText}>따라가라</span>
-        <span className={S.UserTagText}>#KR 123</span>
+        <span className={S.UserNameText}>{game_name}</span>
+        <span className={S.UserTagText}>#{tag_line}</span>
       </div>
     </div>
   );
