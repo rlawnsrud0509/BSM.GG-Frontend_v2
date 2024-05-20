4e9ac34bcad76ff9f557356392c824ba46fcc31e
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-20 19:38:20 +0900

    feat: 전적검색 프로필 컨테이너 api 연결

--- src/components/profilePreviewContainer/index.tsx
+++ src/components/profilePreviewContainer/index.tsx
@@ -1,19 +1,35 @@
+"use client";
+
+import { useGetSummonerbaseInfoQuery } from "@/service/summoner/graphql";
 import * as S from "./index.css";
 import UserinfoSection from "./userInfoSection";
 import Link from "next/link";
+import { useGetLolchangProfileInfoQuery } from "@/service/lolchang/graphql";
 
-const ProfilePreviewContainer = () => {
+interface ProfileContainerProperties {
+  type: "summoner" | "this_week" | "record";
+}
+
+const ProfilePreviewContainer = ({ type = "summoner" }: ProfileContainerProperties) => {
+  const { data } =
+    type === "summoner" ? useGetSummonerbaseInfoQuery() : useGetLolchangProfileInfoQuery();
+
   return (
-    <Link className={S.Container} href={"/user/123"}>
+    <Link
+      className={S.Container}
+      href={`user/${data[type].summoner_types[0].game_name}+${data[type].summoner_types[0].tag_line}`}
+    >
       <section className={S.ProfileImgSection}>
         <div className={S.ProfileImg} />
-        <span className={S.LevelTextBox}>Lv 557</span>
+        <span className={S.LevelTextBox}>Lv {data[type].summoner_types[0].level}</span>
       </section>
       <div className={S.userProfileSection}>
-        <UserinfoSection />
+        <UserinfoSection user_count={data[type].user_count} {...data[type].summoner_types[0]} />
         <div className={S.UserTierInfoSection}>
           <div className={S.UserTierImg} />
-          <span className={S.UserTierText}>Platinum 2</span>
+          <span className={S.UserTierText}>
+            {data[type].solo_tier ? data[type].solo_tier : "Unranked"}
+          </span>
         </div>
       </div>
     </Link>
