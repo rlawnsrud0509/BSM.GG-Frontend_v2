5c94c2cf2f55dc276039149a494e3ef5e01cae9a
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-06-24 23:59:29 +0900

    refactor: apollo graphql 초기 설정파일 분리

--- src/components/common/provider/index.tsx
+++ src/components/common/provider/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/apolloclient";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient({
@@ -13,15 +14,9 @@ const Provider = ({ children }: { children: ReactNode }) => {
     },
   });
 
-  const apolloClient = new ApolloClient({
-    uri: `${process.env.NEXT_PUBLIC_BASE_URL}graphql`,
-    cache: new InMemoryCache(),
-    connectToDevTools: true,
-  });
-
   return (
     <QueryClientProvider client={queryClient}>
-      <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
     </QueryClientProvider>
   );
 };
---
+++ src/lib/apolloclient/index.ts
@@ -0,0 +1,23 @@
+import { getItem } from "@/utils/localStorage";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_BASE_URL}graphql`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = getItem("access_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
