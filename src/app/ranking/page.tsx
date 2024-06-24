ec341060b87513c9f46aa9ba2e1b9cff4fb60d28
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-06-25 00:35:08 +0900

    feat: 랭킹 페이지 컴포넌트 분리

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,15 +1,14 @@
 import SchoolRankHeaderContainer from "@/components/schoolRankHeaderContainer";
 import * as S from "./page.css";
-import SchoolRankUserContainer from "@/components/schoolRankUserContainer";
+import RankInfoSection from "@/components/rankInfoSection";
+import { Suspense } from "react";
 
 const Ranking = () => {
   return (
     <div className={S.Container}>
       <span className={S.PageText}>교내랭킹</span>
       <SchoolRankHeaderContainer />
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((e, i) => (
-        <SchoolRankUserContainer rank={e} key={`dummy2${i}`} />
-      ))}
+      <RankInfoSection />
     </div>
   );
 };
