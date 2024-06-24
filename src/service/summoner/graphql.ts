24e91640b2748851b16a386e40a0397e3294fff3
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-06-24 23:56:54 +0900

    feat: 소환사 정보 graphql 쿼리문 추가

--- src/service/summoner/graphql.ts
+++ src/service/summoner/graphql.ts
@@ -1,29 +1,25 @@
-import { getItem } from "@/utils/localStorage";
+import { UserInfoSectionProperties } from "@/types/components/userInfoSectionProperties.type";
 import { gql, useSuspenseQuery } from "@apollo/client";
 
 export const useGetSummonerbaseInfoQuery = () => {
-  const game_name = getItem("game_name");
-  const tag_line = getItem("tag_line");
-
-  const { data } = useSuspenseQuery<any>(gql`
-        query {
-            summoner(game_name: "${game_name}", tag_line: "${tag_line}") {
-              summoner_types {
-                game_name,
-                tag_line,
-                profile_icon,
-                level,
-                ranking,
-                rank_point,
-                name,
-                grade,
-                class_no,
-                student_no
-              },
-              user_count
-            }
-        }
-    `);
+  const { data } = useSuspenseQuery<UserInfoSectionProperties>(gql`
+    query {
+      getSummoner {
+        gameName
+        tagLine
+        profileIcon
+        level
+        ranking
+        soloPoint
+        soloTier
+        name
+        grade
+        classNo
+        studentNo
+        userCount
+      }
+    }
+  `);
 
   return { data };
 };
