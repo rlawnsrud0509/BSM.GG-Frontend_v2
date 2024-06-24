5d037e019c93e1df621f796529c1adf657a7d387
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-06-25 00:42:37 +0900

    feat: 메인 검색바 검색기능 추가

--- src/components/recordSearchInput/index.tsx
+++ src/components/recordSearchInput/index.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import * as S from "./index.css";
 import { SearchIcon } from "@/style/base/svg";
+import { useRouter } from "next/navigation";
 
 const RecordSearchInput = () => {
+  const [inputData, setInputData] = useState("");
+  const router = useRouter();
+
   return (
     <div className={S.Container}>
       <section className={S.InputSection}>
-        <input className={S.SearchInput} placeholder=" '소환사명 + #태그' 로 검색하세요!" />
-        <div className={S.SearchIconBox}>
+        <input
+          className={S.SearchInput}
+          placeholder="hide on bush #kr1"
+          onChange={(e) => setInputData(e.target.value)}
+        />
+        <div
+          className={S.SearchIconBox}
+          onClick={() =>
+            router.push(`/user/${inputData.split("#")[0].trim()}-${inputData.split("#")[1].trim()}`)
+          }
+        >
           <SearchIcon />
         </div>
       </section>
