b11bff0920b9a76f352be7c771e5f3fbeb905573
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-06-25 00:37:50 +0900

    feat: 아이템 리스트 데이터 핸들링 로직 추가

--- src/components/common/ItemListBox/index.css.ts
+++ src/components/common/ItemListBox/index.css.ts
@@ -1,6 +1,6 @@
 import { theme } from "@/style/base/theme/index.css";
 import { Row } from "@/style/ui/Flex";
-import { style } from "@vanilla-extract/css";
+import { createVar, style } from "@vanilla-extract/css";
 
 export const Container = style(
   Row({
@@ -13,6 +13,34 @@ export const ItemImg = style({
   width: "3rem",
   height: "3rem",
 
+  position: "relative",
+  overflow: "hidden",
+
   borderRadius: "0.5rem",
-  backgroundColor: theme.gray[200],
+  backgroundColor: theme.gray[900],
+});
+
+export const EmptyItemImg = style({
+  width: "3rem",
+  height: "3rem",
+
+  position: "relative",
+  overflow: "hidden",
+
+  borderRadius: "0.5rem",
+  backgroundColor: theme.gray[900],
+  opacity: 0.2,
+});
+
+export const WardImg = style({
+  width: "3rem",
+  height: "3rem",
+
+  position: "relative",
+  overflow: "hidden",
+
+  borderRadius: "0.5rem",
+  backgroundColor: theme.gray[900],
+
+  marginLeft: "0.5rem",
 });
