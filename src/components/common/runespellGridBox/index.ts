cec35c8bba5123a47c6050951e9315257eedbab9
김준경 <64942256+rlawnsrud0509@users.noreply.github.com>
2024-07-04 02:40:21 +0900

    Create index.ts

---
+++ src/components/common/runespellGridBox/index.ts
@@ -0,0 +1,23 @@
+import * as S from "./index.css";
+import { RunespellGridboxProperties } from "@/types/components/common/RunespellGridboxProperties.type";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+
+const RunespellGridBox = ({ runeSpellList, containerSize }: RunespellGridboxProperties) => {
+  return (
+    <div className={S.Container}>
+      {runeSpellList.map((e, i) => (
+        <img
+          key={`RuneSpell${i}`}
+          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${i < 2 ? "spell" : "perk"}/${e.id}.png`}
+          alt="Rune & Spell Img"
+          className={S.SummonerSpellImg}
+          style={assignInlineVars({
+            [S.containerSize]: `${i !== 3 ? containerSize : containerSize * 0.6}rem`,
+          })}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RunespellGridBox;
